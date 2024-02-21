c975783fc540a278c80b430ef84ac7510f7e3213
COv879 <c.olaveherrera@gmail.com>
2024-02-21 18:07:36 -0300

    Updated commit

--- extractRating.py
+++ extractRating.py
@@ -3,5 +3,5 @@ import re
 text = "Rated 4.3 out of 5 stars based on 265 reviews."
 textrevised = re.findall(r"\d\.\d", text)
 ftext = textrevised[0]
-floattext = float(textrevised)
-print(type(floattext))
+print(type(ftext))
+print(ftext)
